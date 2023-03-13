9e32e1003e36a770336d8a7bdfc26bfccbeeeaf6
izambtx <94102754+izambtx@users.noreply.github.com>
2023-03-13 14:45:31 +0700

    Update index.php

--- app/Views/header/index.php
+++ app/Views/header/index.php
@@ -11,6 +11,7 @@
 
     <title>One Point Lesson</title>
 
+    <link rel="icon" type="img/x-icon" href="/img/favicon.ico">
     <!-- Custom fonts for this template-->
     <link href="<?php base_url(); ?>/vendor/fontawesome-free/css/all.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
