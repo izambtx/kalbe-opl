3b8e8b81b65d287cdbf351b87e547c540e528595
izambtx <94102754+izambtx@users.noreply.github.com>
2023-03-19 23:30:05 +0700

    Update create.php

--- app/Views/admin/create.php
+++ app/Views/admin/create.php
@@ -27,6 +27,7 @@
                                 <form class="user" action="<?= url_to('register') ?>" method="post">
                                     <?= csrf_field() ?>
 
+                                    <input type="hidden" name="user_image" id="user_image" value="default.gif"></input>
                                     <div class="form-group">
                                         <input type="number" class="form-control bg-light form-control-user <?php if (session('errors.NIK')) : ?>is-invalid<?php endif ?>" name="NIK" placeholder="NIK" value="<?= old('NIK') ?>" autofocus>
                                     </div>
@@ -44,14 +45,18 @@
                                             <input type="password" class="form-control bg-light form-control-user <?php if (session('errors.pass_confirm')) : ?>is-invalid<?php endif ?>" id="exampleRepeatPassword" name="pass_confirm" placeholder="<?= lang('Auth.repeatPassword') ?>" autocomplete="off">
                                         </div>
                                     </div>
-                                    <div class="input-group mb-3">
-                                        <select class="custom-select bg-light" id=" inputGroupSelect01">
-                                            <option selected hidden>Choose Level...</option>
-                                            <option value="1">Admin</option>
-                                            <option value="2">User</option>
-                                            <option value="3">Atasan</option>
-                                            <option value="4">Engineer</option>
-                                        </select>
+                                    <div class="form-group mb-5">
+                                        <div class="input-group mb-3">
+                                            <select class="<?php if (session('validation.distribusi')) : ?>is-invalid<?php endif ?> custom-select" id="distribusi" name="distribusi">
+                                                <option selected disabled hidden>Choose Department...</option>
+                                                <?php foreach ($distribusi as $d) : ?>
+                                                    <option value="<?= $d['id'];  ?>" <?= old('distribusi') == $d['id'] ? 'selected' : '' ?>><?= $d['nama_distribusi'];  ?></option>
+                                                <?php endforeach; ?>
+                                            </select>
+                                            <div class="invalid-feedback">
+                                                <?= session('validation.distribusi') ?>
+                                            </div>
+                                        </div>
                                     </div>
                                     <button type="submit" class="font-weight-bold btn btn-success btn-user btn-block">
                                         REGISTER
